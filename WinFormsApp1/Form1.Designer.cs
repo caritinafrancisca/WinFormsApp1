293d5112e184b0130612701445c278bb5ce04d44
2024.4.12 <2024.4.12@DESKTOP-CUTRVDB>
2025-05-14 19:44:59 -0400

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            bbtnAbrirArquivo = new Button();
+            button1 = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            saveFileDialog1 = new SaveFileDialog();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(82, 94);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(591, 325);
+            textBox1.TabIndex = 0;
+            // 
+            // bbtnAbrirArquivo
+            // 
+            bbtnAbrirArquivo.Location = new Point(311, 41);
+            bbtnAbrirArquivo.Name = "bbtnAbrirArquivo";
+            bbtnAbrirArquivo.Size = new Size(115, 23);
+            bbtnAbrirArquivo.TabIndex = 1;
+            bbtnAbrirArquivo.Text = "Salvar Como...";
+            bbtnAbrirArquivo.UseVisualStyleBackColor = true;
+            bbtnAbrirArquivo.Click += bbtnAbrirArquivo_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(104, 41);
+            button1.Name = "button1";
+            button1.Size = new Size(139, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Abrir arquivo...";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(bbtnAbrirArquivo);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button bbtnAbrirArquivo;
+        private Button button1;
+        private OpenFileDialog openFileDialog1;
+        private SaveFileDialog saveFileDialog1;
+    }
+}
